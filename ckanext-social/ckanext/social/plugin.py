123873b9ee19152e167fcc23304f31234097f330
Edmond Atto <edmond091@gmail.com>
2017-09-18 15:54:58 +0300

    [Chore] Fetch all helpers and make them available to the templates

--- ckanext-social/ckanext/social/plugin.py
+++ ckanext-social/ckanext/social/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class SocialPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +12,17 @@ class SocialPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'social')
+
+    def get_helpers(self):
+        """
+        All functions, not starting with __ in the ckanext.social.lib
+        module will be loaded and made available as helpers to the
+        templates.
+        """
+        from ckanext.social.lib import helpers
+        from inspect import getmembers, isfunction
+
+        helper_dict = {}
+
+        funcs = [o for o in getmembers(helpers, isfunction)]
+        return dict([(f[0],f[1],) for f in funcs if not f[0].startswith('__')])
