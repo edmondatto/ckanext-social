7c52576c643ff03ca0605fd07489bf1814d9a745
peterpaints <musonyengigi@gmail.com>
2017-10-03 11:36:58 +0300

    [fix] Have setup.py read long description from README.md and not .rst which has been deleted

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 from codecs import open  # To use a consistent encoding
 from os import path
+
+from setuptools import (find_packages,  # Always prefer setuptools over distutils
+                        setup)
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
